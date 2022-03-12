1652b052cd46d28d5262d6f00fb8d3274a802f7d
kaveh <ka7eh@pm.me>
2022-03-12 16:35:06 +0100

    Handle permissions for docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,9 +2,23 @@
 
 set -e
 
+echo $HOST_USER
+echo $HOST_UID
+
+if [[ $HOST_USER && $HOST_UID ]]; then
+  id -u "$HOST_USER" &>/dev/null || adduser --disabled-password --no-create-home --uid "$HOST_UID" "$HOST_USER"
+  chown -R "$HOST_USER":"$HOST_USER" /ctsm-api/resources
+  export USER="$HOST_USER"
+fi
+
+sudo -s -u "$USER" bash <<EOF
+
 cd /ctsm-api
 
 ./scripts/setup_ctsm.sh
 ./scripts/migrations_forward.sh
 
+celery -A app.tasks worker -E --loglevel INFO &
 uvicorn app.main:app --reload --host 0.0.0.0
+
+EOF
