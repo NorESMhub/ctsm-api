095cae7fb38c4885d415761faa8c6ffb4ebb571e
kaveh <ka7eh@pm.me>
2022-04-06 15:24:00 +0200

    Update CORS settings

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1)
