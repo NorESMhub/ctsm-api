bb5393c94bc0d73a7ed3bb8d841b99648d3a7c5e
kaveh <ka7eh@pm.me>
2022-04-21 15:19:51 +0200

    Fix entrypoint_setup.sh

--- docker/entrypoint_setup.sh
+++ docker/entrypoint_setup.sh
@@ -10,8 +10,9 @@ if [[ $HOST_USER && $HOST_UID ]]; then
   chown -R "$HOST_USER":"$HOST_USER" /ctsm-api/resources
   USER="$HOST_USER"
   HOME="/home/$HOST_USER"
+fi
 
-  cat >>/home/$USER/.bashrc <<EOF
+cat >>"$HOME"/.bashrc <<EOF
 
 export CIME_MACHINE=container
 export MPICC=mpicc
@@ -29,6 +30,4 @@ export MPIFC=mpif90
 export MPIF90=mpif90
 export MPIF77=mpif77
 
-ln -fs /ctsm-api/resources/dotcime /$HOME/.cime
-
-fi
+ln -fs /ctsm-api/resources/dotcime "$HOME"/.cime
