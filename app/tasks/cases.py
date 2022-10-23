0f08cd2a0e966537b1ab6742035a089489360ced
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 09:20:15 +0000

    Adding tarfile member sanitization to extractall()

--- app/tasks/cases.py
+++ app/tasks/cases.py
@@ -82,7 +82,26 @@ def create_case(case: models.CaseModel) -> str:
     if not cesm_data_root.exists():
         response = requests.get(case.data_url, stream=True)
         with tarfile.open(fileobj=io.BytesIO(response.raw.read()), mode="r") as f:
-            f.extractall(path=str(cesm_data_root))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, path=str(cesm_data_root))
 
     logger.info(
         f"Creating case {case.id} with the following attributes:\n"
